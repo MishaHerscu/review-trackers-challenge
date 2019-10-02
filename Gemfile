83545d01ba3b97199822423e21c073fd145515b2
Misha Herscu <mishaherscu@Mishas-MacBook-Pro.local>
2019-10-01 23:56:27 -0500

    colorize test outputs in terminal

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 # Gemfile
 source 'https://rubygems.org'
 ruby '2.6.1'
+
+gem 'colorize'
--- tests.rb
+++ tests.rb
@@ -1,14 +1,18 @@
 # Testing Framework
 
 # Sources to test
+require 'colorize'
+
 require_relative 'test_cases'
 
 # Run tests and print results
 review_tests.each do |test|
   test_result = test[:data].send(test[:test_method]) == test[:result]
   if test_result
-    puts test[:label] + ' passed!'
+    success_string = test[:label] + ' passed!'
+    puts success_string.green
   else
-    puts test[:label] + ' failed!'
+    failure_string = test[:label] + ' failed!'
+    puts failure_string.red
   end
 end
