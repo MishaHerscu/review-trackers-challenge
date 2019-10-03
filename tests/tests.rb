262807427b3a3fdab08f7d330e2bb015ff6802e5
Misha Herscu <mishaherscu@Mishas-MacBook-Pro.local>
2019-10-03 03:27:04 -0500

    formatted testing page

--- tests/tests.rb
+++ tests/tests.rb
@@ -3,12 +3,13 @@
 # Sources to test
 require 'colorize'
 
+# Import test sections
 require_relative 'test_cases/request_uri_test_cases'
 require_relative 'test_cases/review_test_cases'
 require_relative 'test_cases/reviews_request_test_cases'
 require_relative 'test_cases/api_test_cases'
 
-# function for running tests
+# Function for running through test cases within each section
 def run_tests(test_cases, section_iter)
   test_iter = 0
   puts section_iter.to_s + '. _____ ' + test_cases[:label] + ' _____'
@@ -26,20 +27,29 @@ def run_tests(test_cases, section_iter)
   end
 end
 
-# array of imported tests
-test_library = [
-  request_uri_test_cases,
-  review_test_cases,
-  reviews_request_test_cases,
-  api_test_cases
-]
 
-# actually run the tests
-section_iter = 0
-puts "\n"
-test_library.each do |test_cases|
-  section_iter += 1
-  run_tests(test_cases, section_iter)
-  puts "\n\n"
+# Function for running all tests and printing out results
+def run_all_tests
+
+  # Array of test sections
+  test_library = [
+    request_uri_test_cases,
+    review_test_cases,
+    reviews_request_test_cases,
+    api_test_cases
+  ]
+
+  # Actually run tests and print out results
+  section_iter = 0
+  puts "\n"
+  test_library.each do |test_section|
+    section_iter += 1
+    run_tests(test_section, section_iter)
+    puts "\n\n"
+  end
+  puts "\n"
+
 end
-puts "\n"
+
+# Run the tests
+run_all_tests
